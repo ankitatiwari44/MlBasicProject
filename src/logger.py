5431778a49e405870d2933cba144397a20a4f006
ankitatiwari44 <t.ankita0110@gmail.com>
2025-04-15 15:36:52 +0530

    data transformation done

--- src/logger.py
+++ src/logger.py
@@ -2,16 +2,17 @@ import logging
 import os
 from datetime import datetime
 
-# Log file name with timestamp
-LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
-logs_path = os.path.join(os.getcwd(), "logs",LOG_FILE)
-os.makedirs(logs_path, exist_ok=True)
+# Create logs directory
+LOG_DIR = os.path.join(os.getcwd(), "logs")
+os.makedirs(LOG_DIR, exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
-# Logging configuration
+# Create a log file with a timestamp
+LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
+
+# Configure logging
 logging.basicConfig(
-    filename=LOG_FILE,
+    filename=LOG_FILE_PATH,
     format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
     level=logging.INFO,
 )
-
